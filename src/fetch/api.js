551d8f6b02025c05eac0749787b461dfc4041ef4
wuzd@mybilin.cn <wuzd@mybilin.cn>
2018-12-17 09:14:53 +0800

    代理服务器

---
+++ src/fetch/api.js
@@ -0,0 +1,21 @@
+  import axios from 'axios'
+
+  export function fetch(url, params) {
+    return new Promise((resolve, reject) => {
+      axios.post(url, params).then(
+        response => {
+          resolve(response.data)
+        }
+      ).catch(error => {
+        console.log(error)
+        reject(error)
+      })
+    })
+  }
+
+  getDefaultData=()=>{
+    return fetch('/api/getBoardList');
+  }
+  export default {
+    getDefaultData
+  }
