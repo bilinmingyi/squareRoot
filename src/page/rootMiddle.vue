3969ef864d69063ba3e6e4c9b7f4f01414ac3640
wuzd@mybilin.cn <wuzd@mybilin.cn>
2018-12-17 12:06:19 +0800

    新增处方事件

--- src/page/rootMiddle.vue
+++ src/page/rootMiddle.vue
@@ -5,18 +5,100 @@
 </template>
 
 <script>
-  import { mapActions } from 'vuex'
+  import {mapActions, mapState} from 'vuex'
+
   export default {
     name: "rootMiddle",
-    data(){
-      return {
-
-      }
+    data() {
+      return {}
     },
-    methods:{
+    computed: {
+      ...mapState({
+        recipeList: state => state.recipeList
+      })
+    },
+    created(){
+      this.addNewRecipt(1);
+      this.addNewRecipt(2);
+    },
+    methods: {
       ...mapActions([
         'add_new_recipt'
       ]),
+      addNewRecipt(type) {
+        let item = {};
+        switch (type) {
+          case 1://重要处方
+            item = {
+              type: 1,
+              money: 0,
+              data: {
+                is_cloud: 0,
+                category: "1",
+                doctor_remark: "",
+                dosage: 0,
+                usage: "",
+                frequency: "",
+                extra_price: 0,
+                extra_num: 0,
+                extra_feetype_id: "",
+                items: []
+              }
+            };
+            break;
+          case 2://中成药处方
+            item = {
+              type: 2,
+              money: 0,
+              data: {
+                doctor_remark: "",
+                items: []
+              }
+            };
+            break;
+          case 3://产品处方
+            item = {
+              type: 3,
+              money: 0,
+              data: {
+                is_cloud: 0,
+                doctor_remark: "",
+                items: []
+              }
+            }
+            break;
+          case 4://项目处方
+            item = {
+              type: 5,
+              money: 0,
+              data: {
+                doctor_remark: "",
+                items: []
+              }
+            }
+            break;
+          case 5://附加服务
+            item = {
+              type: 5,
+              money: 0,
+              data: {
+                doctor_remark: "",
+                items: []
+              }
+            }
+            break;
+          case 6://材料处方
+            item = {
+              type: 6,
+              money: 0,
+              data: {
+                doctor_remark: "",
+                items: []
+              }
+            }
+        }
+        this.add_new_recipt(item)
+      }
 
     }
   }
