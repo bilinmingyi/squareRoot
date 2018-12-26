6bab21119bca5c7d1012cc6bc07205e2205c56aa
wuzd@mybilin.cn <wuzd@mybilin.cn>
2018-12-26 14:57:25 +0800

    保存修改

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'dyyzs/public/squareRoot',
     assetsPublicPath: '/',
 
     /**
