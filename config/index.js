263bca891f5cb38e90d09b22b11af1f36da0b082
wuzd@mybilin.cn <wuzd@mybilin.cn>
2019-03-28 16:26:28 +0800

    打包配置

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'dyyzs/public/squareRoot',
+    assetsSubDirectory: 'yzshis/public/squareRoot',
     assetsPublicPath: '/',
 
     /**
